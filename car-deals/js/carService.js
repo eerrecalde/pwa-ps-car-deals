2156d11a56a5a4827e6872f706103e5b668bc214
Emiliano Errecalde <eerrecalde@gmail.com>
2017-01-23 15:14:30 +0000

    Offline site module. Completed.

--- car-deals/js/carService.js
+++ car-deals/js/carService.js
@@ -3,15 +3,32 @@ define(['./template.js', './clientStorage.js'], function(template, clientStorage
   var apiUrlLatest = apiUrlPath + 'latest-deals.php';
   var apiUrlCar = apiUrlPath + 'car.php?carId=';
   function loadMoreRequest() {
-    fetch(apiUrlLatest + '?carId=' + clientStorage.getLastCarId())
-    .then(function(response) {
-      return response.json();
+    fetchPromise()
+    .then(function(status){
+      document.getElementById("connection-status").innerHTML = status;
+      loadMore();
     })
-    .then(function(data) {
-      clientStorage.addCars(data.cars)
-      .then(function() {
-        loadMore();
-      });
+  }
+
+  function fetchPromise() {
+    return new Promise(function(resolve, reject) {
+      fetch(apiUrlLatest + '?carId=' + clientStorage.getLastCarId())
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        clientStorage.addCars(data.cars)
+        .then(function() {
+          data.cars.forEach(preCacheDetailsPage)
+          resolve("The connection is OK, showing latest results");
+        });
+      })
+      .catch(function(e) {
+        resolve("No connection, showing offline results")
+      })
+      setTimeout(function() {
+        resolve("The connection is hanging, showing offline results");
+      }, 3000)
     })
   }
 
@@ -32,6 +49,19 @@ define(['./template.js', './clientStorage.js'], function(template, clientStorage
     .catch(function(){
       alert("Oops, can't retrieve page");
     })
+  }
+
+  function preCacheDetailsPage(car) {
+    if('serviceWorker' in navigator) {
+      var carDetailsUrl = apiUrlCar + car.value.details_id;
+      window.caches.open('carDealsCachePagesV1').then(function(cache) {
+        cache.match(carDetailsUrl).then(function(response) {
+          if(!response) {
+            cache.add(new Request(carDetailsUrl))
+          }
+        })
+      })
+    }
   }
 
   return {
